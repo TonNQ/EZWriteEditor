318601a7177f54d26b920ad3c30862d006e67a17
TonNQ <nguyenquoctoan12122003@gmail.com>
2025-02-11 15:54:10 +0700

    config proj

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,102 @@
+import path from 'path'
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import tseslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['node_modules', 'dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      prettier: prettierPlugin,
+      react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      import: importPlugin,
+      'jsx-a11y': jsxA11y,
+      '@typescript-eslint': tseslint.plugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        node: {
+          paths: [path.resolve()],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        },
+      }
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-no-target-blank': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'no-unused-vars': ['warn', { vars: 'all', args: 'after-used', ignoreRestSiblings: true }],
+      'import/no-unused-modules': ['warn', { unusedExports: true }],
+      'prettier/prettier': [
         'warn',
-        { allowConstantExport: true },
+        {
+          arrowParens: 'always',
+          semi: false,
+          trailingComma: 'none',
+          tabWidth: 2,
+          endOfLine: 'auto',
+          useTabs: false,
+          singleQuote: true,
+          printWidth: 120,
+          jsxSingleQuote: true,
+        },
+      ],
+      'object-shorthand': 'off',
+      'object-curly-spacing': ['warn', 'always'],
+      'max-len': 'off',
+      'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+      'no-underscore-dangle': 'off',
+      'no-param-reassign': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { vars: 'all', args: 'none' }],
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      '@typescript-eslint/indent': 'off',
+      'react/jsx-key': 'error',
+      'react/jsx-props-no-spreading': 'off',
+      'react/jsx-boolean-value': 'off',
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-wrap-multilines': 'off',
+      'react/destructuring-assignment': 'off',
+      'react/require-default-props': 'off',
+      'react/no-array-index-key': 'off',
+      'react/jsx-curly-newline': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      'import/prefer-default-export': 'off',
+      'import/no-cycle': 'off',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            ['^react', '^next', '^@?\w'],
+            ['^@/'],
+            ['^\.'],
+          ],
+        },
       ],
     },
-  },
+  }
 )
